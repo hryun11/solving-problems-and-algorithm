de45f72ec3c8c9b7f3a57b128296483efab027f5
HRYUN <yunhyerim41@gmail.com>
2023-01-28 03:52:28 +0900

    2023-01-28

---
+++ programmers/Level 1/004 자릿수 더하기/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int solution = s.solution(987);
+
+        System.out.println(solution);
+    }
+}
