457d83585fe3ceafe1d850aafa6b267866ab7bcf
HRYUN <yunhyerim41@gmail.com>
2023-01-30 19:29:05 +0900

    2023-01-30

---
+++ programmers/Level 1/003 평균 구하기/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {1, 2, 3, 4};
+        double solution = s.solution(arr);
+
+        System.out.println(solution);
+    }
+}
